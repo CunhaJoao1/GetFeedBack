dc4122ac6d9729fa3e33212991a298f1d3164d5c
João Victor Cunha <joaovictorcunha8@gmail.com>
2022-06-09 23:41:09 -0300

    Choose options of feedback with usestate

--- src/Compoents/WidgetForm.tsx
+++ src/Compoents/WidgetForm.tsx
@@ -1,33 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { CloseButton } from './CloseButton'
 import  bugIcon from "../Assets/bug.svg"
 import  ideaIcon from "../Assets/idea.svg"
 import ThoughtIcon from "../Assets/thought.svg"
 
-export function WidgetForm() {
-    const feedbackTypes = {
-        BUG:{
-            title: "Problema",
-            image:{
-                source: bugIcon,
-                alt: 'Imagem de um inseto',
-            },
-        },
-        IDEA:{
-            title: "Ideia",
-            image:{
-                source: ideaIcon,
-                alt: 'Imagem de um inseto',
+
+const feedbackTypes = {
+    BUG:{
+        title: "Problema",
+        image:{
+            source: bugIcon,
+            alt: 'Imagem de um inseto',
         },
     },
-        THOUGHT:{
-            title: "Outro",
-            image:{
-                source: ThoughtIcon,
-                alt: 'Imagem de um inseto',
-                },
-        },
-    };
+    IDEA:{
+        title: "Ideia",
+        image:{
+            source: ideaIcon,
+            alt: 'Imagem de um inseto',
+    },
+},
+    THOUGHT:{
+        title: "Outro",
+        image:{
+            source: ThoughtIcon,
+            alt: 'Imagem de um inseto',
+            },
+    },
+};
+type FeedBackType = keyof typeof feedbackTypes;
+
+export function WidgetForm() {
+   const [feedbackType, setFeedbackType] = useState<FeedBackType | null>(null)
   return (
     <div className='bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] sm:w-auto'>
         <header>
@@ -38,9 +42,9 @@ export function WidgetForm() {
         <main className='flex py-8 gap-2 w-full'>
             {Object.entries(feedbackTypes).map(([key, value]) =>{
                 return (
-                    <button 
+                    <button key={key}
                     className=' flex flex-col items-center bg-zinc-800 rounded-lg py-5 w-24 flex-1 gap-2 border-2 border-transparent hover:border-brand-500 focus:to-brand-500 focus:outline-none'
-                    
+                    onClick={()=>setFeedbackType(key as FeedBackType)}
                     >
                         <img src={value.image.source} alt={value.image.alt} />
                        <span>{value.title}</span>
