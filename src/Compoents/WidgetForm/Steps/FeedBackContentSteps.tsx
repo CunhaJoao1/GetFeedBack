4e8ee1d61fad83edfece4f80d325cf07fb6d802f
João Victor Cunha <joaovictorcunha8@gmail.com>
2022-06-10 21:51:53 -0300

    feedback step layout

--- src/Compoents/WidgetForm/Steps/FeedBackContentSteps.tsx
+++ src/Compoents/WidgetForm/Steps/FeedBackContentSteps.tsx
@@ -1,4 +1,4 @@
-import { ArrowLeft } from 'phosphor-react'
+import { ArrowLeft, Camera } from 'phosphor-react'
 import React from 'react'
 import { CloseButton } from '../../CloseButton'
 import { FeedBackType, feedbackTypes } from '../Index'
@@ -27,9 +27,27 @@ export function FeedBackContentSteps(props: feedbackContentStepsProps) {
             <CloseButton/>
         </header> 
 
-        <main className='flex py-8 gap-2 w-full'>
+        <form className='my-4 w-full'>
+        <textarea
+            className='min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 resize-none focus:outline-none scrollbar scrollbar-thumb-zinc-700 scrollbar-track-tranparent scrollbar-thin' 
+            placeholder='Conte com detalhes o que está acontecendo...'
+        />
+        <footer className='flex gap-2 mt-2'>
+            <button 
+                type='submit'
+                className='p-2 bg-zinc-800 rounded-md border-transparent hover:bg-zinc-800 transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500'
+            >
+                <Camera className='w-6 h-6'/>
+            </button>
+            <button
+                type='submit'
+                className='p-2 bg-brand-500 rounded-md border-transparent flex flex-1 w-full justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors'
+            >
+                Enviar feedback
+            </button>
+        </footer>
+    </form>
 
-        </main>
     </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,17 @@ module.exports = {
     extend: {
       colors:{
         brand:{
+          300: '#996DFF',
           500: '#8257E5',
         }
+      },
+      borderRadius:{
+        md: '4px'
       }
     },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('tailwind-scrollbar'),
+  ],
 }
