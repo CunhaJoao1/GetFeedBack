220876f3454feaa27732d8ff5fb8cda058914db4
João Victor Cunha <joaovictorcunha8@gmail.com>
2022-06-10 22:32:08 -0300

    screen shot button working

---
+++ src/Compoents/WidgetForm/ScreenShotBotton.tsx
@@ -0,0 +1,46 @@
+import { Camera, Trash } from 'phosphor-react'
+import React, { useState } from 'react'
+import html2canvas from 'html2canvas'
+import Loading from './Loading'
+
+
+type screenshotProps = {
+    screenShot: string;
+    onScreenShotTook: (screenShot: string | null) => void;
+
+}
+export default function ScreenShotButton(props: screenshotProps) {
+    const [isTakingScreenShot, SetIsTakingScreenShot] = useState(false)
+    async function handleTakeScreenShot(){
+        SetIsTakingScreenShot(true)
+        const canvas = await html2canvas(document.querySelector('html')!)
+        const base64image = canvas.toDataURL('image/png')
+
+        props.onScreenShotTook(base64image)
+        SetIsTakingScreenShot(false)
+
+    }
+    if(props.screenShot){
+        return (
+            <button
+                type='button'
+                className='p-1 w-10 h-10 rounded-md border-transparent flex justify-end items-end text-zinc-400 hover:text-zinc-100 transition-colors'
+                onClick={()=>props.onScreenShotTook(null)}
+                style={{
+                    backgroundImage: `url(${props.screenShot})`
+                }}
+            >
+                <Trash width={'fill'}/>
+            </button>
+        )
+    }
+  return (
+    <button 
+        type='submit'
+        className='p-2 bg-zinc-800 rounded-md border-transparent hover:bg-zinc-700 transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500'
+        onClick={handleTakeScreenShot}
+    >
+        {isTakingScreenShot? <Loading/> : <Camera className='w-6 h-6'/>}
+    </button>
+  )
+}
--- src/Compoents/WidgetForm/Steps/FeedBackContentSteps.tsx
+++ src/Compoents/WidgetForm/Steps/FeedBackContentSteps.tsx
@@ -1,7 +1,8 @@
 import { ArrowLeft, Camera } from 'phosphor-react'
-import React from 'react'
+import React, { useState } from 'react'
 import { CloseButton } from '../../CloseButton'
 import { FeedBackType, feedbackTypes } from '../Index'
+import ScreenShotButton from '../ScreenShotBotton'
 
 
 type feedbackContentStepsProps ={
@@ -10,6 +11,7 @@ type feedbackContentStepsProps ={
 }
 export function FeedBackContentSteps(props: feedbackContentStepsProps) {
     const feedbackTypeInfo = feedbackTypes[props.feedbackType];
+    const [screnShot, setScreenShot] = useState<string | null>(null)
 
   return (
     <>
@@ -27,18 +29,16 @@ export function FeedBackContentSteps(props: feedbackContentStepsProps) {
             <CloseButton/>
         </header> 
 
-        <form className='my-4 w-full'>
+        <main className='my-4 w-full'>
         <textarea
             className='min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 resize-none focus:outline-none scrollbar scrollbar-thumb-zinc-700 scrollbar-track-tranparent scrollbar-thin' 
             placeholder='Conte com detalhes o que está acontecendo...'
         />
         <footer className='flex gap-2 mt-2'>
-            <button 
-                type='submit'
-                className='p-2 bg-zinc-800 rounded-md border-transparent hover:bg-zinc-800 transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500'
-            >
-                <Camera className='w-6 h-6'/>
-            </button>
+            <ScreenShotButton
+                screenShot={screnShot}
+                onScreenShotTook={setScreenShot}
+            />
             <button
                 type='submit'
                 className='p-2 bg-brand-500 rounded-md border-transparent flex flex-1 w-full justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors'
@@ -46,7 +46,7 @@ export function FeedBackContentSteps(props: feedbackContentStepsProps) {
                 Enviar feedback
             </button>
         </footer>
-    </form>
+    </main>
 
     </>
   )
