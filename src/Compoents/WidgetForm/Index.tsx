8caf05318fa74ce12894b0bbb7eb16eae6a9d26e
João Victor Cunha <joaovictorcunha8@gmail.com>
2022-06-10 23:46:24 -0300

    Success step conclude

--- src/Compoents/WidgetForm/Index.tsx
+++ src/Compoents/WidgetForm/Index.tsx
@@ -4,6 +4,7 @@ import  ideaIcon from "../../Assets/idea.svg"
 import ThoughtIcon from "../../Assets/thought.svg"
 import FeedBackTypeSteps from './Steps/FeedBackTypeSteps'
 import { FeedBackContentSteps } from './Steps/FeedBackContentSteps'
+import { FeedBackSuccesStep } from './Steps/FeedBackSuccesStep'
 
 
 export const feedbackTypes = {
@@ -33,18 +34,30 @@ export type FeedBackType = keyof typeof feedbackTypes;
 
 export function WidgetForm() {
    const [feedbackType, setFeedbackType] = useState<FeedBackType | null>(null)
+   const [feedbackSent, setFeedbackSent] = useState(false)
    function handleRestartFeedback(){
+       setFeedbackSent(false)
        setFeedbackType(null)
    }
   return (
     <div className='bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] sm:w-auto'>
-        {!feedbackType? (
+        {feedbackSent?( <FeedBackSuccesStep onFeedbackRestartRequested={handleRestartFeedback}/>
+        
+        ):(
+            <>
+                {!feedbackType? (
             <FeedBackTypeSteps
             oncFeedbackTypeChanged = {setFeedbackType}
             />
-        ): <FeedBackContentSteps feedbackType = {feedbackType} 
-        onFeedbackRestartRequested = {handleRestartFeedback}
-        />}
+            ): (<FeedBackContentSteps 
+            feedbackType = {feedbackType} 
+            onFeedbackRestartRequested = {handleRestartFeedback}
+            onFeedbackSent = {()=> setFeedbackSent(true)}
+            />
+            )}
+            </>
+        )}       
+        
 
         <footer className='text-neutral-400'>
             Feito com 💙 por <a href="" className='underline underline-offset-2'>Victor Cunha</a> 
